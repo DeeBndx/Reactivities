71336a84e152b2007225791fa7fa93d97b41508b
Danny Bendix Christensen <db@monosoft.dk>
2021-04-20 13:06:07 +0200

    End of Section 3

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,10 +11,10 @@ namespace API.Controllers
   public class ActivitiesController : BaseApiController
   {
     private readonly DataContext _context;
+
     public ActivitiesController(DataContext context)
     {
       _context = context;
-
     }
 
     [HttpGet]
