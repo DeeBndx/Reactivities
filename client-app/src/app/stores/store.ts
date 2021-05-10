677a2b2333e057fa71881cb7bcfa99defa0f212d
DeeBndx <danny.christensen2000@gmail.com>
2021-05-10 09:03:41 +0200

    End of Section 10

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,12 +1,16 @@
+import { create } from "node:domain";
 import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
+import CommonStore from "./commonStore";
 
 interface Store {
-  activityStore: ActivityStore
+  activityStore: ActivityStore;
+  commonStore: CommonStore;
 }
 
 export const store: Store = {
-  activityStore: new ActivityStore()
+  activityStore: new ActivityStore(),
+  commonStore: new CommonStore(),
 }
 
 export const StoreContext = createContext(store);
@@ -14,6 +18,5 @@ export const StoreContext = createContext(store);
 export function useStore() {
   return useContext(StoreContext);
 }
-
 
 
